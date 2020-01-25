05e2855a908cf3a40638d1e677f92286c69f2f45
Asilbek5999 <asilbek.aliev.1999@mail.ru>
2020-01-25 22:10:08 +0500

    Generate Question model viaa artisan

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Questions(){
+        return $this->hasMany(Question::class);
+    }
 }
