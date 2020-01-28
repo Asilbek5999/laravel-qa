6fe1cb5aca87467311aef53c531fc7b313a49cbd
Asilbek <asilbek.aliev.1999@mail.ru>
2020-01-28 23:10:01 +0500

    display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -14,12 +14,17 @@ class Answer extends Model
     {
         return $this->belongsTo(User::class);
     }
-        public static function boot()
-        {
-            parent::boot();
-            static::created(function($answer){
-                $answer->question->increment('answers_count');
-                $answer->question->save();
-            });
-        }
+    public static function boot()
+    {
+        parent::boot();
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
+    }
+
+    public function getCreatedDateAttribute()
+    {
+    	return $this->created_at->diffForHumans();
+    }
 }
